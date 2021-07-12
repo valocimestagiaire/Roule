10e4b1aa2d60b41caecd6022d15a6edc4eb89c7f
valocimestagiaire <85479880+valocimestagiaire@users.noreply.github.com>
2021-07-12 17:04:08 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>VALOCIME/ Application Badges - Accueil</title>
+		<meta charset="UTF-8">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		<link href="style.css"/ rel="stylesheet">
+	</head>
+	<body>
+	
+	<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+		<div class="navbar w-100 flex-md-column">
+			<div class="ml-auto">
+				<a class="btn btn-primary" style="background-color: #E9D001;" href="#"><b>GAINS SALARIÉS</b><br>(Bien être / souplesse / ...)</a>
+				<a class="btn btn-primary" style="background-color: #0ACE00;" href="#"><b>ENVIRONNEMENT</b><br>(RSE / impacts / ...)</a>
+				<a class="btn btn-primary" style="background-color: #8401D9;" href="#"><b>ADMINISTRATIF</b><br>(Process / contrats / ...)</a>
+				<a class="btn btn-primary" style="background-color: pink;" href="#"><b>RESSOURCES HUMAINES</b></a>
+			</div>
+			<br>
+			<div class="ml-auto">
+				<a class="btn btn-primary" style="background-color: #0188E9;" href="#"><b>GAINS ENTREPRISE</b><br>(Financier / image / ...)</a>
+				<a class="btn btn-primary" style="background-color: #FF8500;" href="#"><b>CIVISME</b></a>
+				<a class="btn btn-primary" style="background-color: #954E00;" href="#"><b>JURIDIQUE</b><br>(Contrats / ...)</a>
+				<a class="btn btn-primary" style="background-color: #DC0000;" href="#"><b>SÉCURITÉ</b></a>
+			</div>
+			<br>
+			<div class="ml-auto">
+				<a class="btn btn-dark ovale" href="#"><b>OFFRES</b></a>
+				<a class="btn btn-dark ovale" href="#"><b>TROTTINETTES</b></a>
+				<a class="btn btn-dark ovale" href="#"><b>MATÉRIEL</b></a>
+				<a class="btn btn-dark ovale" href="#"><b>ASSURANCE</b></a>
+			</div>
+		</div>
+	</nav>
+	
+	<h1>A</h1><br>
+	<h1>B</h1><br>
+	<h1>C</h1><br>
+	<h1>D</h1><br>
+	<h1>E</h1><br>
+	<h1>F</h1><br>
+	<h1>G</h1><br>
+	<h1>H</h1><br>
+	<h1>I</h1><br>
+	<h1>J</h1><br>
+	<h1>K</h1><br>
+	<h1>L</h1><br>
+	<h1>M</h1><br>
+	<h1>N</h1><br>
+	<h1>O</h1><br>
+	<h1>P</h1><br>
+	
+	
+	<footer class="page-footer fixed-bottom">
+		<div class="div-btn">
+			<a class="btn btn-dark ms-left ovale btn-footer" href="#"><b>SOMMAIRE</b></a>
+		</div>
+	</footer>
+		
+	</body>
+</html>
